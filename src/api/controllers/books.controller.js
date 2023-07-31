f404bcbe0b9466169eec2c530b4461a1db83b08d
= <=>
2023-08-01 00:38:37 +0500

    populated is worked

--- src/api/controllers/books.controller.js
+++ src/api/controllers/books.controller.js
@@ -1,39 +1,39 @@
 const { v4: uuid } = require("uuid");
 const path = require("path");
 const books = require("../models/books.schema");
+const authors = require("../models/author.schema"); // Import the Author schema
 
 const createBook = async (req, res) => {
   try {
     const { image } = req.files;
-    const {
-      title,
-      pages,
-      year,
-      price,
-      country,
-      author,
-      description,
-      author_id,
-    } = req.body;
-    console.log(req.body);
-    const imageName = `${uuid()}${path.extname(image.name)}`;
-    console.log(imageName);
+    const { title, pages, year, price, country, author, description } =
+      req.body;
 
+    const imageName = `${uuid()}${path.extname(image.name)}`;
     image.mv(process.cwd() + "/uploads/" + imageName);
 
-    await books.create({
+    let authorData = await authors.findOne({ firstName: author }).populate();
+    console.log(authorData);
+
+ 
+    if (!authorData) {
+      return res.status(404).json({message: "no author found"})
+    }
+
+    
+    const data = await books.create({
       title,
       pages,
       year,
       price,
       country,
-      author,
+      author: authorData,
       description,
-      author_id,
       imageName,
     });
 
-    res.status(200).json({ msg: "Successfully created book!" });
+     console.log(data)
+    res.status(200).json({ msg: "Successfully created book!", data: data });
   } catch (error) {
     res.json({ msg: error.message });
   }
